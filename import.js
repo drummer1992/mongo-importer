0905c6397f96fd14d5ba9facaba50bce46ed7959
Andrii_Varlamov <andrii.varlamov@backendless.consulting>
2020-07-26 15:07:25 +0300

    - remove redundant error handlers - changed default output

--- import.js
+++ import.js
@@ -8,6 +8,7 @@ const { MongoClient } = require('mongodb')
 const DEFAULT_URL = 'mongodb://localhost:27017'
 
 const OPERATIONS = [
+  'count',
   'find',
   'findOne',
   'insertOne',
@@ -54,22 +55,28 @@ const errorHandler = error => {
 
 rl.question = message => new Promise(resolve => question.call(rl, message, answer => resolve(answer)))
 
-const invokeRequest = async (collection, isFind) => {
-  const operation = isFind
-    ? 'find'
+const invokeRequest = async (collection, isCount) => {
+  const operation = isCount
+    ? 'count'
     : await rl.question('Which operation?')
 
-  const query = isFind
+  const query = isCount
     ? {}
     : parseQuery(await rl.question('Print query like: foo=\'bar\', baz=42\n'))
 
   assert(OPERATIONS.includes(operation), 'Not valid operation: ' + operation)
 
-  const response = operation === 'find'
-    ? await collection[operation](query).toArray()
-    : await collection[operation](query)
+  let output
 
-  console.dir(response, { depth: Infinity })
+  if (operation === 'count') {
+    output = await collection.find(query).count()
+  } else if (operation === 'find') {
+    output = await collection[operation](query).toArray()
+  } else {
+    output = await collection[operation](query)
+  }
+
+  console.dir(isCount ? { countOfInsertedDocuments: output } : output, { depth: Infinity })
 }
 
 async function run() {
@@ -89,18 +96,18 @@ async function run() {
   const client = await MongoClient.connect(
     customUrl || DEFAULT_URL,
     { useUnifiedTopology: true },
-  ).catch(errorHandler)
+  )
 
   const db = client.db(dbName)
   const collection = db.collection(collectionName)
 
-  await collection.insertMany(getJson(file)).catch(errorHandler)
+  await collection.insertMany(getJson(file))
 
   await rl.question('File already imported, do you need some mongo request?')
-    ? await invokeRequest(collection).catch(errorHandler)
-    : await invokeRequest(collection, true).catch(errorHandler)
+    ? await invokeRequest(collection)
+    : await invokeRequest(collection, true)
 
   await client.close()
 }
 
-run().catch(console.error)
+run().catch(errorHandler).finally(() => process.exit())
